0a03d5b3ac6b60a2682436a162a3031c4271597e
Daiana Ghisio <daianaghisio@gmail.com>
2022-07-31 15:42:33 -0300

    Entrega Front End

---
+++ FrontEnd/src/app/Model/persona.model.ts
@@ -0,0 +1,16 @@
+
+export class persona{
+
+    id?: number;
+    nombre: String;
+    apellido: String;
+    img: String;
+
+    constructor(nombre: string, apellido: string, img: string){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.img = img;
+    }
+
+    
+}
